f8d86d3307da6baae14d50cf957c238eadc92e34
Milena <jessicaa.milena@gmail.com>
2021-10-26 22:02:15 -0400

    pintf exercise and numberToWords

---
+++ numberWords/NumberToWords.java
@@ -0,0 +1,10 @@
+package com.numberWords;
+
+public class NumberToWords {
+    public static void NumberToWords(int number){
+        if (number < 0){
+            System.out.println("Invalid Value");
+        }
+
+    }
+}
