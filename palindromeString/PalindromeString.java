ae91790bb10f2577011244cf4df75519d0141b62
Milena <jessicaa.milena@gmail.com>
2021-10-27 21:16:39 -0400

    commit palindromeString execise

--- palindromeString/PalindromeString.java
+++ palindromeString/PalindromeString.java
@@ -1,2 +1,18 @@
-package com.palindromeString;public class PalindromeString {
+package com.palindromeString;
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String palindrome = new StringBuffer(A).reverse().toString();
+
+        if(A.equals(palindrome)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
 }
